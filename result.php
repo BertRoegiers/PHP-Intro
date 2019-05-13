9df5aed06a1579abb561f796f695637c72b7d5c2
Bert Roegiers <bertroegiers@gmail.com>
2019-05-13 12:27:42 +0200

    first commit

---
+++ result.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias aliquid eveniet possimus quam voluptates. Aut cupiditate dolorum explicabo itaque molestias nihil odit recusandae repellat suscipit ut. Aliquam maiores pariatur tempore?</p>
+
+<p>
+    <?php
+
+    echo $_POST["favouriteMovies"];
+
+    ?>
+
+
+</p>
+<p>
+    <?php
+
+
+    echo $_POST["favouriteTvShows"];
+    ?>
+
+
+</p>
+</body>
+</html>
---
+++ setup.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+
+
+
+    <form action ="result.php"  method="post">
+        What are your favourite Tv-shows :
+        <input type="text" name="favouriteTvShows"><br>
+        <input type="text" name="favouriteMovies"><br>
+
+        What are your favourite movies :
+
+        <input type="submit">
+    </form>
+
+
+
+
+<?php
+?>
+
+</body>
+</html>
